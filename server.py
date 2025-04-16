8b53a4e8c2da0b61afc9497e35f30a8f53936f54
Teodor Potancok <teodor.potancok@gmail.com>
2025-04-16 12:01:36 +0200

    Make code style cleaner

--- server.py
+++ server.py
@@ -8,51 +8,64 @@ import secrets
 
 from websockets.asyncio.server import serve
 
-global server_socket
+steam_socket = None
 message_map = {}
 
-def make_handler(server_secret: str):
-    global server_socket
-    global message_map
 
+def make_handler(rpc_secret: str):
     async def handler(websocket):
-        async for message in websocket:
-            if message == ('init:' + server_secret):
-                server_socket = websocket
+        global steam_socket
 
-                print('SteamyRPC initialized!')
+        async for message in websocket:
+            if message == ("init:" + rpc_secret):
+                if steam_socket:
+                    print("Error - Double init message detected")
+                else:
+                    steam_socket = websocket
+                    print("SteamyRPC initialized!")
             else:
-                if websocket == server_socket:
-                    print('Received message from Steam:', message)
+                if websocket == steam_socket:
+                    print("Received message from Steam:", message)
 
                     msg = json.loads(message)
-                    id = msg['messageId']
+                    id = msg["messageId"]
 
                     if id in message_map:
-                        msg.pop('messageId', None)
-                        print('Returned value to client:', msg)
-                        message_map[id].send(json.dumps(msg))
-
+                        msg.pop("messageId", None)
+                        print("Returned value to client:", msg)
+                        await message_map[id].send(json.dumps(msg))
                 else:
                     msg = json.loads(message)
+                    id = msg["messageId"]
+
                     message_map[id] = websocket
 
-                    await server_socket.send(json.dumps({
-                        'messageId': msg.id,
-                        'secret': server_secret,
-                        'command': msg.command,
-                        'args': msg.args
-                    }))
+                    await steam_socket.send(
+                        json.dumps(
+                            {
+                                "messageId": id,
+                                "secret": rpc_secret,
+                                "command": msg["command"],
+                                "args": msg["args"],
+                            }
+                        )
+                    )
 
-                    print('Forwarded command to Steam:', message)
+                    print("Forwarded command to Steam:", message)
+
     return handler
 
-def make_payload(port: int, server_secret: str):
-    payload = ''
-    with open('client.template.js', 'r') as file:
+
+def make_payload(port: int, rpc_secret: str):
+    payload = ""
+    with open("client.template.js", "r") as file:
         for line in file.readlines():
-            payload += line.replace(r'{{PORT}}', str(port)).replace(r'{{SECRET}}', server_secret)
+            payload += line.replace(r"{{PORT}}", str(port)).replace(
+                r"{{SECRET}}", rpc_secret
+            )
+
         return payload
+
 
 async def send_payload(debugger_url: str, payload: str):
     async with websockets.connect(debugger_url) as ws:
@@ -61,30 +74,28 @@ async def send_payload(debugger_url: str, payload: str):
         command = {
             "id": msg_id,
             "method": "Runtime.evaluate",
-            "params": {
-                "expression": payload,
-                "awaitPromise": True
-            }
+            "params": {"expression": payload, "awaitPromise": True},
         }
 
         await ws.send(json.dumps(command))
 
+
 async def main():
-    server_secret = secrets.token_urlsafe(16)
+    rpc_secret = secrets.token_urlsafe(16)
     port = 7355
 
-    print('Starting SteamyRPC...')
+    print("Starting SteamyRPC...")
 
-    server = await serve(make_handler(server_secret), "", port)
+    server = await serve(make_handler(rpc_secret), "", port)
 
-    tabs = requests.get('http://localhost:8080/json').json()
+    tabs = requests.get("http://localhost:8080/json").json()
 
     for tab in tabs:
-        if tab['title'] == 'SharedJSContext':
-            debugger_url = tab['webSocketDebuggerUrl']
-            print('Sending payload to:', debugger_url)
+        if tab["title"] == "SharedJSContext":
+            debugger_url = tab["webSocketDebuggerUrl"]
+            print("Sending payload to:", debugger_url)
 
-            payload = make_payload(port, server_secret)
+            payload = make_payload(port, rpc_secret)
 
             await send_payload(debugger_url, payload)
             break
@@ -93,4 +104,4 @@ async def main():
 
 
 if __name__ == "__main__":
-   asyncio.run(main())
+    asyncio.run(main())
