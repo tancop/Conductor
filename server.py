916d58569bfb6ff5006e9a04b308a4c632c07ca7
Teodor Potancok <teodor.potancok@gmail.com>
2025-04-18 13:56:01 +0200

    Add license

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+# server.py
+# (c) 2025 Teodor Potancok
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import asyncio
 import json
 import requests
