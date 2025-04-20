df3ce25c92d415ab3bd275850f4605f8eeef3bb3
Teodor Potancok <teodor.potancok@gmail.com>
2025-04-20 14:11:27 +0200

    Add run scripts

---
+++ run.sh
@@ -0,0 +1,6 @@
+if [ ! -f "~/.steam/steam/.cef-enable-remote-debugging"]; then
+    # enable Steam remote debugging
+    touch "~/.steam/steam/.cef-enable-remote-debugging"
+fi
+
+python ./server.py
