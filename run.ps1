dfa812feb57b1b57baefa9986a2c02d0725312ea
Teodor Potancok <teodor.potancok@gmail.com>
2025-05-07 21:42:32 +0200

    Clean up unused code

--- run.ps1
+++
@@ -1,6 +0,0 @@
-if (!(Test-Path "C:\Program Files (x86)\Steam\.cef-enable-remote-debugging")) {
-    # enable Steam remote debugging
-    Start-Process -Verb RunAs powershell.exe -Args "-NoProfile -ExecutionPolicy Bypass -Command `"New-Item -Path 'C:\Program Files (x86)\Steam\.cef-enable-remote-debugging' -ItemType File`""
-}
-
-python .\main.py
--- run.sh
+++
@@ -1,6 +0,0 @@
-if [ ! -f "~/.steam/steam/.cef-enable-remote-debugging"]; then
-    # enable Steam remote debugging
-    touch "~/.steam/steam/.cef-enable-remote-debugging"
-fi
-
-python ./main.py
